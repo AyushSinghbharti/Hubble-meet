d350aa5a15a0f4dbf5c5fc3d2b8abf570e95efab
Ayush Singh <ayushsinghbharti123@gmail.com>
2025-06-20 14:11:01 +0530

    Start mergin both setup-basic-branch and dev-ios branch with merge conflic fixed (Nearly)

--- src/app/(tabs)/(chat)/chat.tsx
+++ src/app/(tabs)/(chat)/chat.tsx
@@ -48,9 +48,8 @@ const RenderCard = ({ item }: { item: any }) => {
       style={styles.chatCard}
       onPress={() =>
         router.push({
-          pathname: `chatStack/listing/${item.id}`,
-          // pathname: `subScreen/(chatStack)/listing/${item.id}`,
-          params: { item: JSON.stringify(item) }, //Look out for error in future maybe!!!
+          pathname: `chatStack/${item.id}`,
+          params: { item: JSON.stringify(item) },
         })
       }
     >
