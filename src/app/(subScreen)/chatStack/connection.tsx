c860cee327b83089144cd110ce3337d8463edafc
Ayush Singh <ayushsinghbharti123@gmail.com>
2025-06-29 15:28:25 +0530

    fixed connection screen on chatpage

--- src/app/(subScreen)/chatStack/connection.tsx
+++ src/app/(subScreen)/chatStack/connection.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   View,
   Text,
@@ -7,11 +7,45 @@ import {
   ScrollView,
   StatusBar,
 } from "react-native";
-import { Ionicons, MaterialIcons } from "@expo/vector-icons";
+import { Entypo, Ionicons, MaterialIcons } from "@expo/vector-icons";
 import VbcCard from "../../../components/VbcCard";
 import { useRouter } from "expo-router";
+import { Pressable, TextInput } from "react-native-gesture-handler";
 
 export default function Connections() {
+  const [viewSeach, setViewSearch] = useState(false);
+  const [searchText, setSearchText] = useState("");
+  const [viewPopUp, setPopUp] = useState(false);
+
+  //Renders
+  const PopUpMenu = () => {
+    return (
+      <Pressable
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          flex: 1,
+          width: "100%",
+          height: "100%",
+        }}
+        onPress={() => setPopUp(!viewPopUp)}
+      >
+        <View style={styles.popupContainer}>
+          <TouchableOpacity>
+            <Text style={styles.popupText}>Invite a Friend</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text style={styles.popupText}>Contacts</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text style={styles.popupText}>Refresh</Text>
+          </TouchableOpacity>
+        </View>
+      </Pressable>
+    );
+  };
+
   const router = useRouter();
   return (
     <View style={styles.container}>
@@ -19,20 +53,56 @@ export default function Connections() {
 
       {/* Header */}
       <View style={styles.header}>
-        <TouchableOpacity
-          onPress={() => router.back()}
-          style={{ marginRight: 16 }}
-        >
-          <Ionicons name="arrow-back" size={24} color="black" />
-        </TouchableOpacity>
+        {viewSeach ? (
+          <View style={styles.searchContainer}>
+            <TouchableOpacity
+              onPress={() => setViewSearch(!viewSeach)}
+              style={{ marginRight: 16 }}
+            >
+              <Ionicons name="arrow-back" size={24} color="black" />
+            </TouchableOpacity>
+            <TextInput
+              value={searchText}
+              onChangeText={(t) => setSearchText(t)}
+              placeholder="Seach"
+              placeholderTextColor={"#aaa"}
+              style={styles.searchText}
+            />
+            {searchText && (
+              <Entypo
+                name="cross"
+                size={24}
+                color="black"
+                onPress={() => setSearchText("")}
+              />
+            )}
+          </View>
+        ) : (
+          <>
+            <TouchableOpacity
+              onPress={() => router.back()}
+              style={{ marginRight: 16 }}
+            >
+              <Ionicons name="arrow-back" size={24} color="black" />
+            </TouchableOpacity>
 
-        <Text style={styles.title}>Connections</Text>
+            <Text style={styles.title}>Connections</Text>
+          </>
+        )}
 
         <View style={styles.headerIcons}>
-          <TouchableOpacity style={styles.iconButton}>
-            <Ionicons name="search" size={22} color="black" />
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.iconButton}>
+          {!viewSeach && (
+            <TouchableOpacity
+              style={styles.iconButton}
+              onPress={() => setViewSearch(!viewSeach)}
+            >
+              <Ionicons name="search" size={22} color="black" />
+            </TouchableOpacity>
+          )}
+          <TouchableOpacity
+            style={styles.iconButton}
+            onPress={() => setPopUp(!viewPopUp)}
+          >
             <MaterialIcons name="more-vert" size={22} color="black" />
           </TouchableOpacity>
         </View>
@@ -40,10 +110,11 @@ export default function Connections() {
 
       {/* Scrollable Content */}
       <View style={styles.scrollArea}>
-        <VbcCard
-          spacing={20}
-        />
+        <VbcCard spacing={20} />
       </View>
+
+      {/* PopupMenu */}
+      {viewPopUp && <PopUpMenu />}
     </View>
   );
 }
@@ -60,7 +131,20 @@ const styles = StyleSheet.create({
     paddingHorizontal: 16,
     paddingBottom: 12,
     flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
   },
+  searchContainer: {
+    flex: 1,
+    paddingHorizontal: 12,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderWidth: 1,
+    borderColor: "#000",
+    borderRadius: 25,
+  },
+  searchText: { fontSize: 14, fontFamily: "InterSemiBold", flex: 1 },
   title: {
     fontSize: 18,
     fontFamily: "InterSemiBold",
@@ -76,5 +160,24 @@ const styles = StyleSheet.create({
   },
   scrollArea: {
     paddingHorizontal: 12,
+  },
+
+  //Pop Up style
+  popupContainer: {
+    position: "absolute",
+    top: 85,
+    right: 24,
+    justifyContent: "space-evenly",
+    backgroundColor: "#fff",
+    borderRadius: 10,
+    minHeight: 175,
+    minWidth: 275,
+    gap: 18,
+    padding: 26,
+    elevation: 5,
+  },
+  popupText: {
+    fontSize: 16,
+    fontFamily: "InterMedium",
   },
 });
